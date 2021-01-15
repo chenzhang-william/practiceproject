38578460b8516b70e81e208b3d0eccaa97df8503
zhangchen <zhangchen@yealink.com>
2021-01-15 15:43:06 +0800

    Merge remote-tracking branch 'remotes/origin/zhangchen/level1/programming' into HEAD
    # Conflicts:
    #	level1/src/main/java/com/yealink/level1/controller/StaffController.java
    #	level1/src/main/java/com/yealink/level1/domain/StaffMapper.java
    #	level1/src/main/java/com/yealink/level1/service/StaffService.java
    #	level1/src/main/java/com/yealink/level1/service/impl/StaffServiceImpl.java

---
+++ level3/src/main/java/com/yealink/level3/bean/Conference.java
@@ -0,0 +1,44 @@
+package com.yealink.level3.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+/**
+ * @author zhangchen
+ * @description Conference
+ * @date 2021/1/5 17:11
+ */
+@Data
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Conference {
+    @Id
+    @GeneratedValue
+    private String id;
+
+    private String title;
+
+    @NotNull(message = "会议号")
+    private String conferenceNo;
+
+    private String conferenceRoom;
+
+    private String ruleId;
+
+    private String startTime;
+
+    private String endTime;
+
+    private long createTime;
+
+    private long modifyTime;
+}
